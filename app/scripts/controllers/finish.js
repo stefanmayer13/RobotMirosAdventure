0f5324e8f9014f5aca46b1aee3d614ce86bbda44
Stefan Mayer <stefanmayer13@gmail.com>
2014-04-29 00:20:43 +0200

    Added final level

--- app/scripts/controllers/finish.js
+++ app/scripts/controllers/finish.js
@@ -5,7 +5,9 @@ angular.module('RobotMirosAdventure')
     var texts = [
       'Now it gets a little trickier!',
       'Miro needs to lern basic math to continue. Implement the solve function of Miro.',
-      'I hope you know the Tower of Hanoi. Calculate the number of needed steps with the specified disks.'
+      'I hope you know the Tower of Hanoi. Calculate the number of needed steps with the specified disks.',
+      'Not bad, not bad.',
+      'Congratulations, you finished it!!!'
     ];
     Task.get(function (data) {
       $scope.text = texts[data.nr];
